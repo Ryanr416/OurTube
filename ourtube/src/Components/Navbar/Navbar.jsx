bebb7e97cd5163b779b02ce94fbcdd969a6756b4
ryanr416 <ryanrichardson0993@gmail.com>
2024-11-30 11:19:09 -0700

    added more logos

--- ourtube/src/Components/Navbar/Navbar.jsx
+++ ourtube/src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import "./Navbar.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Navbar = () => {
   return (
     <nav className="flex-div">
       <div className="nav-left flex-div">
-        <img src={MenuIcon} alt="" />
-        <img src={YouTubeIcon} alt="" />
+        <img className="menu-icon" src={MenuIcon} alt="" />
+        <img className="logo" src={YouTubeIcon} alt="" />
+      </div>
+
+      <div className="nav-middle flex-div">
+        <input type="text" placeholder="Search" />
+        <img src={SearchIcon} alt="" />
       </div>
     </nav>
   );
