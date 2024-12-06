58fe4c0de3ac711e6a14a75b74d3b2d3a386ba3f
ryanr416 <ryanrichardson0993@gmail.com>
2024-12-06 09:38:30 -0700

    added sidebar icons

--- ourtube/src/Components/Sidebar/Sidebar.jsx
+++ ourtube/src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import "./Sidebar.css";
+import HomeIcon from "@mui/icons-material/Home"; /* Home */
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports"; /* Video Games */
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar"; /* Autos */
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball"; /* Sports */
+import TvIcon from "@mui/icons-material/Tv"; /* entertainment */
+import ComputerIcon from "@mui/icons-material/Computer"; /* Tech */
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import BookIcon from "@mui/icons-material/Book"; /* blog */
+import NewspaperIcon from "@mui/icons-material/Newspaper"; /* news */
 
 const Sidebar = () => {
   return <div></div>;
