ae32f475d17874aaca71a7730d831c438cc504fb
ryanr416 <ryanrichardson0993@gmail.com>
2025-01-09 11:41:28 -0700

    adding side links

--- ourtube/src/Components/Sidebar/Sidebar.jsx
+++ ourtube/src/Components/Sidebar/Sidebar.jsx
@@ -6,12 +6,42 @@ import DirectionsCarIcon from "@mui/icons-material/DirectionsCar"; /* Autos */
 import SportsBasketballIcon from "@mui/icons-material/SportsBasketball"; /* Sports */
 import TvIcon from "@mui/icons-material/Tv"; /* entertainment */
 import ComputerIcon from "@mui/icons-material/Computer"; /* Tech */
-import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic"; /* music */
 import BookIcon from "@mui/icons-material/Book"; /* blog */
 import NewspaperIcon from "@mui/icons-material/Newspaper"; /* news */
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <div className="shortcut-links">
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <div className="side-link">
+          <img src={HomeIcon} alt="" /> <p>Home</p>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
