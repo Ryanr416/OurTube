51ff7c094b6e76205298e6753ec89170c7fbc886
ryanr416 <ryanrichardson0993@gmail.com>
2025-01-28 09:20:49 -0700

    adding feed

--- ourtube/src/Pages/Home/Home.jsx
+++ ourtube/src/Pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Home.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+import Feed from "../../Components/Feed/Feed";
 const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        {/* checking if sidebar is true, if so, it will provide an empty string and if it is false, it will provide a new classname. */}
+
+        <Feed />
+      </div>
     </>
   );
 };
