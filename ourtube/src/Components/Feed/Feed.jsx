cecf279cc1e6b19224e8b35d9685ca27c323a4b5
ryanr416 <ryanrichardson0993@gmail.com>
2025-01-28 09:29:51 -0700

    adding feed functionalty

--- ourtube/src/Components/Feed/Feed.jsx
+++ ourtube/src/Components/Feed/Feed.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Feed.css";
 
 const Feed = () => {
-  return <div></div>;
+  return (
+    <div className="card">
+      <img src={""} alt="" />
+      <h2>best channel</h2>
+      <h3>greatstack</h3>
+      <p>15k views &bull; 2 days ago</p>
+    </div>
+  );
 };
 
 export default Feed;
