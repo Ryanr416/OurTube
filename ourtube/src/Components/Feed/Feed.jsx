61507ca7b18995e5e10db953afa5c02fb1486f56
ryanr416 <ryanrichardson0993@gmail.com>
2025-02-03 09:41:03 -0700

    adding feed page

--- ourtube/src/Components/Feed/Feed.jsx
+++ ourtube/src/Components/Feed/Feed.jsx
@@ -3,11 +3,62 @@ import "./Feed.css";
 
 const Feed = () => {
   return (
-    <div className="card">
-      <img src={""} alt="" />
-      <h2>best channel</h2>
-      <h3>greatstack</h3>
-      <p>15k views &bull; 2 days ago</p>
+    <div className="feed">
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
     </div>
   );
 };
