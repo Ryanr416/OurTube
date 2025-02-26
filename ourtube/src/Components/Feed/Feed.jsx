b7797f6b90d82bcb8a301979e106ad9be3f927c6
ryanr416 <ryanrichardson0993@gmail.com>
2025-02-26 08:00:17 -0700

    added links

--- ourtube/src/Components/Feed/Feed.jsx
+++ ourtube/src/Components/Feed/Feed.jsx
@@ -1,9 +1,65 @@
 import React from "react";
 import "./Feed.css";
+import { Link } from "react-router-dom";
 
 const Feed = () => {
   return (
     <div className="feed">
+      <Link to={`video/20/4521`} className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </Link>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
+
+      <div className="card">
+        <img src={""} alt="" />
+        <h2>best channel</h2>
+        <h3>greatstack</h3>
+        <p>15k views &bull; 2 days ago</p>
+      </div>
       <div className="card">
         <img src={""} alt="" />
         <h2>best channel</h2>
