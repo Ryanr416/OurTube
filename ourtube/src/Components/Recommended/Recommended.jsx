2eef897ab7edd545768ebec4f9b074788e8ef84e
ryanr416 <ryanrichardson0993@gmail.com>
2025-03-24 09:08:47 -0600

    added rec videos

---
+++ ourtube/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Recommended.css";
+
+const Recommended = () => {
+  return (
+    <div className="recommended">
+      <div className="side-video-list">
+        <div className="vid-info">
+          <h4>Reccomended section</h4>
+          <p>great recommended</p>
+          <p>199k views</p>
+        </div>
+      </div>
+
+      <div className="side-video-list">
+        <div className="vid-info">
+          <h4>Reccomended section</h4>
+          <p>great recommended</p>
+          <p>199k views</p>
+        </div>
+      </div>
+
+      <div className="side-video-list">
+        <div className="vid-info">
+          <h4>Reccomended section</h4>
+          <p>great recommended</p>
+          <p>199k views</p>
+        </div>
+      </div>
+
+      <div className="side-video-list">
+        <div className="vid-info">
+          <h4>Reccomended section</h4>
+          <p>great recommended</p>
+          <p>199k views</p>
+        </div>
+      </div>
+
+      <div className="side-video-list">
+        <div className="vid-info">
+          <h4>Reccomended section</h4>
+          <p>great recommended</p>
+          <p>199k views</p>
+        </div>
+      </div>
+
+      <div className="side-video-list">
+        <div className="vid-info">
+          <h4>Reccomended section</h4>
+          <p>great recommended</p>
+          <p>199k views</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recommended;
